59fe8a8cd23c98a0b44808da0c0b40b41973f855
João Teixeira <joaoduarteixeira@gmail.com>
2025-06-17 15:34:17 +0100

    Added search logic

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,29 +1,51 @@
 from django.shortcuts import render
 from django.urls import reverse
 from django.http import HttpResponseRedirect
+from django import forms
 
 import markdown2
 from . import util
 
-def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+class NewTaskForm(forms.Form):
+    search = forms.CharField(label="",widget=forms.TextInput(attrs={'placeholder': 'Search Encyclopedia'}))
+
+def index(request): 
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            search = form.cleaned_data["search"]
+            return entry(request, search)
+    else:    
+        return render(request, "encyclopedia/index.html", {
+            "form":NewTaskForm(),
+            "entries": util.list_entries()
+        })
 
 def entry(request, name):
+    results = []
     for entry in util.list_entries():
         if entry.lower() == name.lower():
             if name != entry:
-                return HttpResponseRedirect(reverse("entry", args=[entry]))
+                return HttpResponseRedirect(reverse("wiki:entry", args=[entry]))
 
             entry_content = markdown2.markdown(util.get_entry(entry))
             return render(request, 'encyclopedia/entry.html', {
+                "form":NewTaskForm(),
                 "entry_content":entry_content,
                 "entry_name": entry
             })
-    else:
-        if name not in util.list_entries():
-            return render(request, 'encyclopedia/error.html', {
-                "name":name,
-            })
+        elif name in entry:
+            results.append(entry)
+    
+    if len(results) == 0:
+        return render(request, 'encyclopedia/error.html', {
+            "form":NewTaskForm(),
+            "name":name,
+        })
+    
+    return render(request, 'encyclopedia/search.html', {
+                "form":NewTaskForm(),
+                "entries":results,
+                "entry_name": name
+            }) 
 
