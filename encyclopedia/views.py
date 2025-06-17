7d03cd4de0010f9daff0f7a4f4f42c5ebd1a989f
João Teixeira <joaoduarteixeira@gmail.com>
2025-06-17 13:22:49 +0100

    Added logic of entries as html pages

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
+import markdown2
 from . import util
-
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def entry(request, name):
+    for entry in util.list_entries():
+        if entry.lower() == name.lower():
+            if name != entry:
+                return HttpResponseRedirect(reverse("entry", args=[entry]))
+
+            entry_content = markdown2.markdown(util.get_entry(entry))
+            return render(request, 'encyclopedia/entry.html', {
+                "entry":entry_content,
+            })
+    else:
+        if name not in util.list_entries():
+            return render(request, 'encyclopedia/error.html', {
+                "name":name,
+            })
 
