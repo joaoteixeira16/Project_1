76db5cbb1fee4ba8ccdcf27f4d3db30c0d2198f8
João Teixeira <joaoduarteixeira@gmail.com>
2025-06-18 14:53:28 +0100

    Fixed search, added edit and random

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = "wiki"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("random/", views.random_entry, name="random_entry"),
     path("wiki/<str:name>", views.entry, name="entry"),
-    path("new/", views.new_entry, name="new_entry")
+    path("new/", views.new_entry, name="new_entry"),
+    path("edit/<str:name>", views.edit_page, name="edit_page")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -3,10 +3,9 @@ from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django import forms
 from django.contrib import messages
-
-
 import markdown2
 from . import util
+import random
 
 class NewSearchForm(forms.Form):
     search = forms.CharField(label="",widget=forms.TextInput(attrs={'placeholder': 'Search Encyclopedia'}))
@@ -19,12 +18,54 @@ class NewPageForm(forms.Form):
         'placeholder': 'Enter Markdown content...',
     }))
 
+class EditPageFormHidden(forms.Form):
+    title = forms.CharField().hidden_widget()
+    content = forms.CharField(widget=forms.Textarea()).hidden_widget()
+
+class EditPageForm(forms.Form):
+    content = forms.CharField(widget=forms.Textarea(attrs={
+        'class': 'form-control',
+        'style': 'max-width: 600px; height: 200px;',
+    }))
+
+
+
 def index(request): 
     if request.method == "POST":
         form = NewSearchForm(request.POST)
         if form.is_valid():
             search = form.cleaned_data["search"]
+            request.method = ""
             return entry(request, search)
+    else:    
+        return render(request, "encyclopedia/index.html", {
+            "form":NewSearchForm(),
+            "entries": util.list_entries()
+        })
+    
+def random_entry(request): 
+    random_entry_name = random.choice(util.list_entries())
+    random_entry_content = markdown2.markdown(util.get_entry(random_entry_name))
+    return render(request, 'encyclopedia/entry.html', {
+        "form":NewSearchForm(),
+        "entry_content":random_entry_content,
+        "entry_name": random_entry_name,
+        "form_edit":EditPageFormHidden()
+    })
+    
+def edit_page(request, name):
+    if request.method == "POST":
+        form_edit = EditPageForm(request.POST)
+        if form_edit.is_valid():
+            content = form_edit.cleaned_data["content"]
+            save = util.save_entry(name,content)
+            request.method = ""
+            return entry(request, name)
+        else:    
+            return render(request, "encyclopedia/edit_page.html", {
+                "form":NewSearchForm(),
+                "form_edit":form_edit,
+            })
     else:    
         return render(request, "encyclopedia/index.html", {
             "form":NewSearchForm(),
@@ -47,6 +88,7 @@ def new_entry(request):
             
             else:
                 save = util.save_entry(title,content)
+                request.method = ""
                 return entry(request, title)
         else:    
             return render(request, "encyclopedia/new_entry.html", {
@@ -61,6 +103,17 @@ def new_entry(request):
     
 
 def entry(request, name):
+    if request.method == "POST":
+        entry_content = util.get_entry(name)
+        
+        form = EditPageForm(initial={'content': entry_content})
+        return render(request, 'encyclopedia/edit_page.html', {
+                "form":NewSearchForm(),
+                "entry_content":entry_content,
+                "entry_name": name,
+                "form_new":form
+            })
+
     results = []
     for entry in util.list_entries():
         if entry.lower() == name.lower():
@@ -71,9 +124,10 @@ def entry(request, name):
             return render(request, 'encyclopedia/entry.html', {
                 "form":NewSearchForm(),
                 "entry_content":entry_content,
-                "entry_name": entry
+                "entry_name": name,
+                "form_edit":EditPageFormHidden()
             })
-        elif name in entry:
+        elif name.lower() in entry.lower():
             results.append(entry)
     
     if len(results) == 0:
